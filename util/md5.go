8eb3feaa44d91ce8ff9164aff639fd59d39eff5d
朱亮 <teamide@163.com>
2023-03-09 18:43:13 +0800

    Update md5.go

--- util/md5.go
+++ util/md5.go
@@ -3,8 +3,8 @@ package util
 import (
 	"crypto/md5"
 	"fmt"
+	"go.uber.org/zap"
 	"io"
-	"log"
 )
 
 // MD5 获取MD5字符串
@@ -13,7 +13,7 @@ func MD5(str string) string {
 	m := md5.New()
 	_, err := io.WriteString(m, str)
 	if err != nil {
-		log.Fatal(err)
+		Logger.Error("MD5 error", zap.Error(err))
 	}
 	arr := m.Sum(nil)
 	return fmt.Sprintf("%x", arr)
