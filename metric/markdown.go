51561665797ecb6d1c60ec9219c253f683bc596d
朱亮 <teamide@163.com>
2023-06-18 12:20:27 +0800

    Update markdown.go

--- metric/markdown.go
+++ metric/markdown.go
@@ -20,22 +20,21 @@ func MarkdownTable(counts []*Count, options *Options) (content string) {
 	if options == nil {
 		options = &Options{}
 	}
-	content += fmt.Sprintf("|                 任务时间                   | 总/成功/失败    |  执行用时 | 累计用时 |TPS |Avg |Min |Max |T50 |T80 | T90 | T99 |  \n")
-	content += fmt.Sprintf("| :------: | :------: | :------: | :------: | :------: |:------: | :------: | :------: | :------: | :------: | :------: | :------: |  \n")
+	content += fmt.Sprintf("|                 任务时间                   | 总/成功/失败    |  任务用时|  执行用时 | 累计用时 |TPS |Avg |Min |Max |T50 |T80 | T90 | T99 |  \n")
+	content += fmt.Sprintf("| :------: | :------: | :------: | :------: | :------: |:------: | :------: | :------: | :------: | :------: | :------: | :------: | :------: |  \n")
 
 	var s string
 	for _, count := range counts {
 
 		content += fmt.Sprintf("|")
 		if options.AddHtmlFormat {
-			s = " %s <br>-<br> %s <br>-<br> %s |"
+			s = " %s <br>-<br> %s |"
 		} else {
-			s = " %s - %s - %s |"
+			s = " %s - %s |"
 		}
 		content += fmt.Sprintf(s,
 			util.TimeFormat(time.UnixMilli(count.StartTime/int64(time.Millisecond)), "2006-01-02 15:04:05.000"),
 			util.TimeFormat(time.UnixMilli(count.EndTime/int64(time.Millisecond)), "2006-01-02 15:04:05.000"),
-			toTime(count.TotalTime/1000000),
 		)
 		if options.AddHtmlFormat {
 			s = " %d <br> <font color='green'>%d</font> <br> <font color='red'>%d</font> |"
@@ -43,6 +42,7 @@ func MarkdownTable(counts []*Count, options *Options) (content string) {
 			s = " %d / %d / %d |"
 		}
 		content += fmt.Sprintf(s, count.Count, count.SuccessCount, count.ErrorCount)
+		content += fmt.Sprintf(" %s |", toTime(count.TotalTime/1000000))
 		content += fmt.Sprintf(" %s |", toTime(count.ExecuteTime/1000000))
 		content += fmt.Sprintf(" %s |", toTime(count.UseTime/1000000))
 		content += fmt.Sprintf(" %s |", count.Tps)
