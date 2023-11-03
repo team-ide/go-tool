f6e3d1c73694c59e54dacac205cb876325b65c9e
朱亮 <teamide@163.com>
2023-11-03 18:23:47 +0800

    Create init.go

---
+++ db/db_dialer/init.go
@@ -0,0 +1,27 @@
+package db_dialer
+
+import (
+	"golang.org/x/crypto/ssh"
+	"net"
+	"time"
+)
+
+func NewSSHDialer(sshClient *ssh.Client) *SSHClientDialer {
+	return &SSHClientDialer{
+		sshClient: sshClient,
+	}
+}
+
+type SSHClientDialer struct {
+	sshClient *ssh.Client
+}
+
+func (d SSHClientDialer) Dial(network, address string) (net.Conn, error) {
+	return d.sshClient.Dial(network, address)
+}
+
+func (d SSHClientDialer) DialTimeout(network, address string, timeout time.Duration) (net.Conn, error) {
+	//_, cancel := context.WithTimeout(context.Background(), timeout)
+	//defer cancel()
+	return d.Dial(network, address)
+}
